22fc96db20f38a558f252a3a15a0c8a44a1ac986
d4vidmanu <david.silva.m@utec.edu.pe>
2024-10-09 00:05:32 -0500

    final final

--- src/db.js
+++ src/db.js
@@ -1,17 +1,17 @@
 import { createPool } from "mysql2/promise";
 
-// export const pool = createPool({
-//   host: process.env.DB_HOST || '107.22.252.129',
-//   user: process.env.DB_USER || 'root',
-//   password: process.env.DB_PASSWORD || 'utec',
-//   database: process.env.DB_DATABASE || 'scooter_management',
-//   port: process.env.DB_PORT || 8006
-// });
-
 export const pool = createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_DATABASE || "scooter_management",
-  port: 3308,
+  host: process.env.DB_HOST || '34.195.53.21',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'utec',
+  database: process.env.DB_DATABASE || 'scooter_management',
+  port: process.env.DB_PORT || 3308
 });
+
+// export const pool = createPool({
+//   host: process.env.DB_HOST || "localhost",
+//   user: process.env.DB_USER || "root",
+//   password: process.env.DB_PASSWORD || "",
+//   database: process.env.DB_DATABASE || "scooter_management",
+//   port: 3308,
+// });
