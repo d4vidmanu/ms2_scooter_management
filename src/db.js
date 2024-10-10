84e331582b7c749b0c24dc4ccbfd16c83a7a9c33
d4vidmanu <david.silva.m@utec.edu.pe>
2024-10-09 23:11:32 -0500

    adadad

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: process.env.DB_HOST || '34.195.53.21',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'utec',
-  database: process.env.DB_DATABASE || 'scooter_management',
-  port: process.env.DB_PORT || 3308
+  host: process.env.DB_HOST || "18.213.167.234",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "utec",
+  database: process.env.DB_DATABASE || "scooter_management",
+  port: process.env.DB_PORT || 3308,
 });
 
 // export const pool = createPool({
